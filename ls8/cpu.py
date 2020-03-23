cc6d061b55589644960db5ec96603ccd3760bc6a
Martin Twum Mensah <martint.mensah@gmail.com>
2020-03-23 15:59:11 +0000

    chore: Complete step 1

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,10 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0
+        self.halt = False
 
     def load(self):
         """Load a program into memory."""
